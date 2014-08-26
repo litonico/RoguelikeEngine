a8e8b989ca9ebdab356dd22f23635bdc7e8c401d
Lito Nicolai <lito.nicolai@gmail.com>
2014-08-25 19:51:25 -0700

    factored out graphics

--- roguelike.py
+++ roguelike.py
@@ -1,71 +1,7 @@
-import sys
 from utility import Pos, N, S, E, W, UP, DN, NONE
 from entities import Entity, Hero
 from tiles import *
-
-class ASCIIRenderer(object):
-    ''' Graphics engine - writes to console '''
-    # Realllly primitive graphics
-
-    def __init__(self, game):
-        self.game = game
-
-    '''
-    # Not ready to include this
-    def setcolor(color, char):
-        styles = {
-                'default': '\x1b[39;49m',
-                'grey': '\x1b[30;0m',
-                'red': '\x1b[31'
-                }
-        return lambda x: \
-            sys.stdout.write(styles[c])
-            sys.stdout.write(char)
-            sys.stdout.write(styles['default'])
-    '''
-
-    def get_user_input(self):
-        return input()
-
-    def generate_buffer(self):
-        '''Blit the sprites in @game (ASCII chars) onto a buffer'''
-        # Write the 'world' to the buffer
-        # Only the current z-index is drawn
-        self.buf = [
-                tile.sprite for tile in self.game.world[self.game.hero.pos.z]
-                ]
-
-        # On top of that, draw items (not implemented)
-
-        # On top of that, draw the characters
-        for entity in game.entities:
-            if entity.pos.z == game.hero.pos.z:
-                self.buf[game.flatten(entity.pos)] = entity.sprite
-
-    def render(self):
-        ''' Draw the buffer to STDOUT '''
-        # Also surrounds the map with '#'
-        
-        #--Border
-        for i in range(self.game.dim_x+1):
-            sys.stdout.write('#')
-        # Draw the actual game
-        for index, char in enumerate(self.buf):
-            # Write newline if neccesary
-            if index % self.game.dim_x == 0: 
-                sys.stdout.write('#') #--Border
-                sys.stdout.write('\n')
-                sys.stdout.write('#') #--Border
-            sys.stdout.write(char)
-
-        #--Border
-        sys.stdout.write('#')
-        sys.stdout.write('\n')
-        for i in range(self.game.dim_x+1):
-            sys.stdout.write('#')
-        sys.stdout.write('\n')
-
-
+from asciirenderer import ASCIIRenderer
 
 class Game(object):
     '''Args: the dimensions (x, y, z) of the map'''
